87586058341ac59e09cba1ac735c50eebaa83fd8
pokeang <pokeang.ung@gmail.com>
2021-10-28 19:20:46 +0900

    add filter user

--- src/components/parts/UserSearchForm.vue
+++ src/components/parts/UserSearchForm.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="user-search-form">
-    Here is UserSearchForm
+    <section>
+      <div class="input-search">
+        <b-field label="Name" horizontal>
+          <b-input v-model="name"></b-input>
+        </b-field>
+        <b-field label="Age" horizontal>
+          <b-input v-model="age"></b-input>
+        </b-field>
+        <b-field label="Sex" horizontal>
+          <b-input v-model="sex"></b-input>
+        </b-field>
+      </div>
+      <div class="float-right">
+        <b-button @click="search">Search</b-button>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import Vue from "vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+  data() {
+    return {
+      name: null,
+      age: null,
+      sex: null
+    };
+  },
+  methods: {
+    search() {
+      const param = {
+        name: this.name,
+        age: this.age,
+        sex: this.sex
+      };
+      this.$emit("search", param);
+    }
+  }
+});
 </script>
 
 <style scoped>
 .user-search-form {
-  height: 60px;
   background-color: yellow;
+  padding: 30px;
+}
+.input-search {
+  width: 90%;
+}
+.float-right {
+  float: right;
+  margin-top: -38px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="container my-5">
-    <user-search-form></user-search-form>
+    <user-search-form @search="searchUser"></user-search-form>
     <b-table :data="users" :columns="columns"></b-table>
+    <b-pagination
+      v-model="pagination.current"
+      :total="pagination.total"
+      :range-before="pagination.rangeBefore"
+      :range-after="pagination.rangeAfter"
+      :order="pagination.order"
+      :size="pagination.size"
+      :simple="pagination.isSimple"
+      :rounded="pagination.isRounded"
+      :per-page="pagination.perPage"
+      :icon-prev="pagination.prevIcon"
+      :icon-next="pagination.nextIcon"
+      @change="handlerPagination"
+      aria-next-label="Next page"
+      aria-previous-label="Previous page"
+      aria-page-label="Page"
+      aria-current-label="Current page">
+  </b-pagination>
   </div>
 </template>
 
@@ -15,7 +33,26 @@ export default Vue.extend({
   name: "Home",
   data() {
     return {
-      users: titanic,
+      users: [{}],
+      tempUsers: titanic,
+      pagination: {
+        total: 0,
+        current: 1,
+        perPage: 20,
+        rangeBefore: 3,
+        rangeAfter: 1,
+        order: "",
+        size: "",
+        isSimple: false,
+        isRounded: false,
+        prevIcon: "chevron-left",
+        nextIcon: "chevron-right"
+      },
+      paramSearch: {
+        name: "",
+        age: null,
+        sex: null
+      },
       columns: [
         {
           field: "name",
@@ -31,6 +68,41 @@ export default Vue.extend({
         }
       ]
     };
+  },
+  methods: {
+    handlerPagination (current: number) {
+      this.pagination.current = current;
+      this.searchUser(this.paramSearch);
+    },
+    searchUser (param: {}) {
+      Object.assign(this.paramSearch, param);
+      if (Object.values(param).length > 0) {
+        this.tempUsers = [];
+        titanic.filter((user, index) => {
+          let isMatch = false;
+          if (this.paramSearch.name) {
+            isMatch = user.name.toLowerCase().includes(this.paramSearch.name.toLowerCase());
+          }
+          if (this.paramSearch.age) {
+            isMatch = this.paramSearch.name ? (isMatch && user.age == this.paramSearch.age) : user.age == this.paramSearch.age;
+          }
+          if (this.paramSearch.sex) {
+            isMatch = (this.paramSearch.name || this.paramSearch.age) ? (isMatch && user.sex == this.paramSearch.sex) : user.sex == this.paramSearch.sex;
+          }
+          if (isMatch) {
+            this.tempUsers.push(user);
+          }
+          if(index + 1 === titanic.length) {
+            this.pagination.total = this.tempUsers.length;
+            if (this.pagination.total > this.pagination.perPage) {
+              this.users = this.tempUsers.splice((this.pagination.current - 1) * this.pagination.perPage,  this.pagination.perPage * this.pagination.current);
+            } else {
+              this.users = this.tempUsers;
+            }
+          }
+        });
+      }
+    }
   }
 });
 </script>
