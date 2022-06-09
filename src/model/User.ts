6604d5cbd90333ffff57557127ebd5add99d2453
pokeang <pokeang.ung@gmail.com>
2022-06-09 23:42:42 +0900

    Update User.ts

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,15 +1,15 @@
 export interface User {
    pclass: number | string;
    survived: number | string;
-   name: string | string;
-   sex: string | string;
+   name: string;
+   sex: string;
    age: number | string;
    sibsp: number | string;
    parch: number | string;
    ticket: number | string;
    fare: number | string;
-   cabin: string | string;
-   embarked: string | string;
+   cabin: string;
+   embarked: string;
    boat: number | string;
    body: string | number;
    'home.dest': string;
